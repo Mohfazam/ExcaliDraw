b773fd6737044aed151a07cf3c268c74ce7c1f1e
Mohfazam <mohd.sarwar.code@gmail.com>
2025-06-09 02:21:13 +0530

    .env Imported

--- apps/http-backend/src/Routes/userAuthRoute/index.ts
+++ apps/http-backend/src/Routes/userAuthRoute/index.ts
@@ -1,16 +1,22 @@
 import express, {Router} from "express";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv"
+import path from "path";
 
-dotenv.config();
+
+dotenv.config({
+    path: path.resolve(__dirname, "../../../../../.env")
+});
 const JWT_SECRET = process.env.JWT_SECRET;
+console.log(JWT_SECRET);
 
 export const userAuthRouter: Router = express.Router();
 
 
 userAuthRouter.get("/health", (req, res) => {
     res.status(200).json({
-        Message: "api/v1/user/health is hitted"
+        Message: "api/v1/user/health is hitted",
+        JWT_SECRET: JWT_SECRET
     });
 });
 

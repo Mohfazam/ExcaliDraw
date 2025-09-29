7e9380bd398d84d79c0d737989c3112f2d347eff
Mohfazam <mohd.sarwar.code@gmail.com>
2025-09-29 18:08:19 +0530

    Email Mandatory

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,62 +1,72 @@
 import express from "express";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common/config";
 import { middleware } from "./middleware";
-import { createRoomSchema, createUserSchema, signinSchema } from "@repo/common/types";
+import {
+  createRoomSchema,
+  createUserSchema,
+  signinSchema,
+} from "@repo/common/types";
 import { prismaClient } from "@repo/db/client";
 
 const app = express();
 
-app.post("/signup", (req, res) => {
+app.post("/signup", async (req, res) => {
+  const parsedData = createUserSchema.safeParse(req.body);
 
-    const data = createUserSchema.safeParse(req.body);
-
-    if(!data.success){
-        return res.json({
-            Message: "Incorrect Inputs"
-        });
-    }
-
-    res.json({
-        userId: "123"
-    })
+  if (!parsedData.success) {
+    return res.json({
+      Message: "Incorrect Inputs",
+    });
+  }
+  await prismaClient.user.create({
+    data: {
+      email: parsedData.data.username,
+      password: parsedData.data.password,
+      name: parsedData.data.name,
+    },
+  });
+  res.json({
+    userId: "123",
+  });
 });
 
 app.post("/signin", (req, res) => {
+  const data = signinSchema.safeParse(req.body);
 
-    const data = signinSchema.safeParse(req.body);
-
-    if(!data.success){
-        return res.json({
-            Message: "Incorrect Inputs"
-        });
-    }
-    const userId = 1;
-
-    const token = jwt.sign({
-        userId
-    }, JWT_SECRET);
-
-    res.json({
-        token
+  if (!data.success) {
+    return res.json({
+      Message: "Incorrect Inputs",
     });
+  }
+  const userId = 1;
+
+  const token = jwt.sign(
+    {
+      userId,
+    },
+    JWT_SECRET
+  );
+
+  res.json({
+    token,
+  });
 });
 
 app.post("/room", middleware, (req, res) => {
-    const data = createRoomSchema.safeParse(req.body);
+  const data = createRoomSchema.safeParse(req.body);
 
-    if(!data.success){
-        return res.json({
-            Message: "Incorrect Inputs"
-        });
-    }
-
-    res.json({
-        roomId: 123123
+  if (!data.success) {
+    return res.json({
+      Message: "Incorrect Inputs",
     });
+  }
 
+  res.json({
+    roomId: 123123,
+  });
 });
 
 app.listen(3001, () => {
-    console.log("Server started on port 3001");
+  console.log("Server started on port 3001");
 });

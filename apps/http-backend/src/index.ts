cff1598dd9fe8f8de1376c46f9a9d369c77afaec
Mohfazam <mohd.sarwar.code@gmail.com>
2025-09-30 12:55:56 +0530

    Frontend

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -132,6 +132,19 @@ app.get("/chats/:roomId", async (req, res) => {
     });
 });
 
+app.get("/room/:slug", async (req, res) => {
+  const slug = req.params.slug;
+  const room = await prismaClient.room.findFirst({
+    where:{
+      slug
+    }
+  });
+
+  res.json({
+    room
+  });
+});
+
 app.listen(3001, () => {
   console.log("Server started on port 3001");
 });

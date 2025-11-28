56fdf96a101b262ee859b690efb4db4389b85b11
Mohfazam <mohd.sarwar.code@gmail.com>
2025-11-28 23:52:26 +0530

    navigation

--- apps/excalidraw-frontend/src/app/page.tsx
+++ apps/excalidraw-frontend/src/app/page.tsx
@@ -1,9 +1,17 @@
-import Image from "next/image";
+"use client"
 
+import { useRouter } from "next/navigation";
 export default function Home() {
+  const Router = useRouter();
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      
+    <div className="flex min-h-screen items-center gap-12 justify-center bg-zinc-50 font-sans dark:bg-black">
+      <button
+      onClick={() => Router.push("/signup")}
+      >Signup</button>
+
+      <button
+      onClick={() => Router.push("/signin")}
+      >signin</button>
     </div>
   );
 }

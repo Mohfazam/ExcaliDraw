168a0031e4fd2e176e2cf7cce3b5221e3f75b29e
Mohfazam <mohd.sarwar.code@gmail.com>
2025-12-05 19:29:52 +0530

    TopBar

--- apps/excalidraw-frontend/src/components/Canvas.tsx
+++ apps/excalidraw-frontend/src/components/Canvas.tsx
@@ -1,25 +1,36 @@
 import { initDraw } from '@/draw';
-import React, { useEffect, useRef } from 'react'
+import { useEffect, useRef } from 'react'
+import { IconButton } from './IconButton';
+import { Circle, Pencil, RectangleHorizontalIcon } from 'lucide-react';
 
 export const Canvas = ({
-    roomId, socket
-}:{
-    roomId: string;
-    socket: WebSocket;
+  roomId, socket
+}: {
+  roomId: string;
+  socket: WebSocket;
 }) => {
-      const canvasRef = useRef<HTMLCanvasElement>(null);
-    
+  const canvasRef = useRef<HTMLCanvasElement>(null);
 
-    useEffect(() => {
 
-        if (canvasRef.current) {
-            initDraw(canvasRef.current, roomId, socket);
-        }
+  useEffect(() => {
 
-    }, [canvasRef, socket]);
+    if (canvasRef.current) {
+      initDraw(canvasRef.current, roomId, socket);
+    }
+
+  }, [canvasRef, socket]);
   return (
-    <div>
-        <canvas ref={canvasRef} width={window.innerWidth} height={window.innerHeight}></canvas>
+    <div className='h-full overflow-hidden w-full'>
+      <canvas ref={canvasRef} width={window.innerWidth} height={window.innerHeight}></canvas>
+      <TopBar />
     </div>
   )
+}
+
+function TopBar() {
+  return <div className='fixed top-2.5 left-[50%] flex gap-4'>
+    <IconButton icon={<Pencil />} onClick={() => { }} />
+    <IconButton icon={<RectangleHorizontalIcon />} onClick={() => { }} />
+    <IconButton icon={<Circle />} onClick={() => { }} />
+  </div>
 }
